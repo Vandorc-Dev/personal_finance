d3c5a3ac0950572be2abdd398bb48e63dbdf5450
Vanderlândio Z. da Rocha <vandodev@gmail.com>
2022-04-13 13:27:15 -0300

    Criado mensagens de erro

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,3 +5,6 @@ mongoose.Promise = global.Promise;
 module.exports = mongoose.connect('mongodb://localhost:27017/db_finance',{useMongoClient:true});
 
 mongoose.Error.messages.general.required = "O campo '{PATH}' é obrigatório!"
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite minímo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite maxímo de '{MAX}'."
+mongoose.Error.messages.Number.enum = "O '{VALUE}' não é válido para o atributo '{PATH}'."
